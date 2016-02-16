05bca6bc217eb0f143aa4b0d43601934f62cf4f8
Miguel Halys <mhalys91@gmail.com>
2016-02-16 11:45:57 +0100

    GridView con customViews

--- app/src/main/java/miguel/chatgo/FriendsFragment.java
+++ app/src/main/java/miguel/chatgo/FriendsFragment.java
@@ -1,13 +1,19 @@
 package miguel.chatgo;
 
+import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.GridLayout;
+import android.widget.GridView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -15,21 +21,26 @@ import com.parse.ParseQuery;
 import com.parse.ParseRelation;
 import com.parse.ParseUser;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import miguel.chatgo.Utils.ImageAdapter;
 import miguel.chatgo.Utils.utilSingleton;
 
 /**
  * Created by Miguel on 1/8/2016.
  */
-public class FriendsFragment extends ListFragment{
+public class FriendsFragment extends android.support.v4.app.Fragment{
     utilSingleton utilidades= utilSingleton.getInstance();
     ParseUser mCurrentUser;
     ParseRelation<ParseUser> mFriendsRelation;
-    List<ParseUser> mUsers;
+    List<ParseUser> mUsers = new ArrayList<>();
     String [] usernames;
     ProgressBar progressBar;
     TextView addFriends_Textview;
+    ImageAdapter imageAdapter;
+    GridView gridview;
+
 
 
 
@@ -37,6 +48,17 @@ public class FriendsFragment extends ListFragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.friendsfragment, container, false);
+
+        gridview = (GridView) rootView.findViewById(R.id.gridview);
+
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(getContext(), "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
 
         progressBar= (ProgressBar) rootView.findViewById(R.id.progressBarListView);
         addFriends_Textview = (TextView) rootView.findViewById(R.id.noFriendsLabel);
@@ -66,17 +88,16 @@ public class FriendsFragment extends ListFragment{
                     mUsers = objects;
                     usernames = new String[objects.size()];
                     //copyUsernamesIntoStringArray(usernames);
-                    utilidades.copyUsernamesIntoStringArray(usernames,mUsers);
+                    utilidades.copyUsernamesIntoStringArray(usernames, mUsers);
                     progressBar.setVisibility(View.GONE);
-                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                            getListView().getContext(),
-                            R.layout.listviewitem,
-                            usernames);
-                    setListAdapter(adapter);
+                    //Introducir n tiles por los n usuarios de la query.
+                    imageAdapter=new ImageAdapter(getActivity(),mUsers);
+                    imageAdapter.refill();
+                    gridview.setAdapter(imageAdapter);
                     if(mUsers.size()!=0)
                         addFriends_Textview.setVisibility(View.INVISIBLE);
                 } else {
-                    utilidades.generateDialog(e.getMessage(),getListView().getContext()).show();
+                    utilidades.generateDialog(e.getMessage(),getActivity().getApplicationContext()).show();
 
                 }
             }
