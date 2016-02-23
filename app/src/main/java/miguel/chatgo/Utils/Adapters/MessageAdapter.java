094ea7867cf0bd5e5503c82698c7d7324bb2e15f
Miguel Halys <mhalys91@gmail.com>
2016-02-23 13:19:42 +0100

    Proyecto fusionado

--- app/src/main/java/miguel/chatgo/Utils/Adapters/MessageAdapter.java
+++ app/src/main/java/miguel/chatgo/Utils/Adapters/MessageAdapter.java
@@ -3,6 +3,7 @@ package miguel.chatgo.Utils.Adapters;
 import android.content.Context;
 import android.media.Image;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import android.widget.TextView;
 
 import com.parse.ParseObject;
 
+import java.util.Date;
 import java.util.List;
 
 import miguel.chatgo.ParseConstants;
@@ -38,16 +40,24 @@ public class MessageAdapter extends ArrayAdapter<ParseObject> {
             holder = new ViewHolder();
             holder.iconImageView = (ImageView) convertView.findViewById(R.id.imagePic);
             holder.textView = (TextView) convertView.findViewById(R.id.senderLabel);
+            holder.timeLabel = (TextView) convertView.findViewById(R.id.timeLabel);
             convertView.setTag(holder);
         } else {
             holder = (ViewHolder) convertView.getTag();
         }
 
         ParseObject message = mMessages.get(position);
+        Date createAt = message.getCreatedAt();
+        long now = new Date().getTime();
+        String convertedDate = DateUtils.getRelativeTimeSpanString(
+                createAt.getTime(),
+                now,
+                DateUtils.DAY_IN_MILLIS).toString();
+        holder.timeLabel.setText(convertedDate);
         if (message.getString(ParseConstants.KEY_FILETYPE).equals(ParseConstants.TYPE_IMAGE)) {
-            holder.iconImageView.setImageResource(R.drawable.ic_action_camera);
+            holder.iconImageView.setImageResource(R.drawable.picbutton);
         } else {
-            holder.iconImageView.setImageResource(R.drawable.ic_action_play_over_video);
+            holder.iconImageView.setImageResource(R.drawable.play);
         }
         holder.textView.setText(message.getString(ParseConstants.KEY_SENDERNAME));
         return convertView;
@@ -56,6 +66,7 @@ public class MessageAdapter extends ArrayAdapter<ParseObject> {
     public static class ViewHolder {
         ImageView iconImageView;
         TextView textView;
+        TextView timeLabel;
 
     }
 }
